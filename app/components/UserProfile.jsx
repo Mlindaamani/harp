1dcde6f39863df25a4d9907e68eb8a700a3961ea
mlindaamani <mlindaadolf@gmail.com>
2025-01-13 02:34:48 +0300

    Added users route

---
+++ app/components/UserProfile.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+export const UserProfile = () => {
+  return (
+    <div>
+      <section className="bg-dark p-3 rounded-2 d-flex gap-5 justify-content-between align-items-center">
+        <h3 className="text-center py-3 text-secondary flex-1">About</h3>
+        <p className="text-light px-5 py-4 fs-3">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi incidunt
+          quis deleniti ullam distinctio repudiandae, obcaecati rem quasi
+          molestias provident? Fuga voluptates illo nam suscipit possimus
+          ducimus, quas, tempore repellendus esse dignissimos a iusto quam
+          molestias! Rem tempore maxime repellat sit eaque consequuntur dolores
+          iure quasi. Ea fuga facilis laboriosam eligendi, nemo dicta cumque sed
+          aut vitae deserunt ex error aliquam saepe tempora reprehenderit quis
+          quae temporibus quod, velit facere atque deleniti aperiam voluptas
+          libero. Praesentium, iste, nam porro excepturi quasi officiis et
+          ducimus facilis veniam obcaecati eveniet? Eaque mollitia, cupiditate
+          neque fugit expedita accusantium repellendus numquam iure est impedit.
+        </p>
+        <p className="text-warning px-5 py-4 fs-3">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi incidunt
+          quis deleniti ullam distinctio repudiandae, obcaecati rem quasi
+          molestias provident? Fuga voluptates illo nam suscipit possimus
+          ducimus, quas, tempore repellendus esse dignissimos a iusto quam
+          molestias! Rem tempore maxime repellat sit eaque consequuntur dolores
+          iure quasi. Ea fuga facilis laboriosam eligendi, nemo dicta cumque sed
+          aut vitae deserunt ex error aliquam saepe tempora reprehenderit quis
+          quae temporibus quod, velit facere atque deleniti aperiam voluptas
+          libero. Praesentium, iste, nam porro excepturi quasi officiis et
+          ducimus facilis veniam obcaecati eveniet? Eaque mollitia, cupiditate
+          neque fugit expedita accusantium repellendus numquam iure est impedit.
+        </p>
+      </section>
+    </div>
+  );
+};
---
+++ app/users/page.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { UserProfile } from "../components/UserProfile";
+
+export default function Users() {
+  return (
+    <div className="mt-5 p-5 rounded-5 hv-100">
+      <UserProfile />
+    </div>
+  );
+}
