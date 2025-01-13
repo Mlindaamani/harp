91e3f472be190e8478d2abf605bb328bf3bfeee6
mlindaamani <mlindaadolf@gmail.com>
2025-01-13 12:29:02 +0300

    Did absolutely nothing

--- app/layout.js
+++ app/layout.js
@@ -11,12 +11,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className="m-0 p-0">
-        <header className="bg-primary text-light p-5 d-flex align-items-center">
+        <header className="bg-success text-light p-5 d-flex align-items-center">
           <HeaderContents metadata={metadata} />
         </header>
         <main>{children}</main>
-        <footer>
-          <p>This is a footer</p>
+        <footer className="p-5 bg-dark text-center">
+          <p className="fw-bold text-light">This is a footer</p>
         </footer>
       </body>
     </html>
--- app/users/page.jsx
+++ app/users/page.jsx
@@ -6,6 +6,10 @@ export default function Users() {
     <div className="mt-5 p-5 rounded-5 hv-100 container">
       <div className="row gap-5 d-flex">
         <UserProfile />
+        <UserProfile />
+        <UserProfile />
+        <UserProfile />
+        <UserProfile />
       </div>
     </div>
   );
