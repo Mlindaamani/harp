89df432acc1fc71bbb5dcf8639099202f778805c
mlindaamani <mlindaadolf@gmail.com>
2025-01-13 17:49:00 +0300

    Created a layout file in users

--- app/page.js
+++ app/page.js
@@ -1,9 +1,3 @@
-import UserProfile from "./users/page";
-
 export default function Home() {
-  return (
-    <div className="vh-100 p-4">
-      <UserProfile />
-    </div>
-  );
+  return <div className="vh-100 p-4"></div>;
 }
