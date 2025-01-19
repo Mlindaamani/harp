e0271fcbcf93b254c3054e2e0d1b4bd64dea5ee3
mlindaamani <mlindaadolf@gmail.com>
2025-01-19 17:01:57 +0300

    Improved the project route

--- app/(root)/projects/page.jsx
+++ app/(root)/projects/page.jsx
@@ -7,22 +7,25 @@ const Projects = () => {
     <div className="text-center bg-success-subtle text-light mt-5 p-4 rounded-4 d-flex flex-column justify-content-center align-items-center">
       <Image
         src="/database.svg"
-        width={300}
-        height={300}
+        width={150}
+        height={150}
         alt="Projects"
         className="flex-fills"
       />
       <h3 className="text-secondary mt-3 p-3">No projects yet!</h3>
       <div>
-        <Link className="btn btn-secondary p-3 m-5" href="/projects/new">
+        <Link
+          className="btn btn-secondary p-3 m-5 d-flex align-items-center justify-content-between rounded-5"
+          href="/projects/new"
+        >
           <Image
-            src="/post.svg"
+            src="/add.svg"
             width={24}
             height={24}
-            className="text-light m-2"
+            className="text-light"
             alt="Add"
           />
-          Add Projet
+          <span>Add Projet</span>
         </Link>
       </div>
     </div>
