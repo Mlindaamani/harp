00ba95866fa4a23af3f1aeff3656bd8e7aff2e47
mlindaamani <mlindaadolf@gmail.com>
2025-01-23 10:19:57 +0300

    Added overflow-hidded to my app wrapper

--- app/(root)/layout.jsx
+++ app/(root)/layout.jsx
@@ -3,7 +3,7 @@ import { SideBar } from "../components/SideBar";
 
 const RootLayout = ({ children }) => {
   return (
-    <div className="container-fluid">
+    <div className="container-fluid overflow-hidden">
       <div className="row justify-content-start g-5">
         <div className="col-auto">
           <SideBar />
